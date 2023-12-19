b00a73c5cb021a3e953c87d18ac85ac865828527
irojabkhan <irojabkhan@gmail.com>
2023-12-20 01:35:25 +0600

    content updated

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <footer className="max-w-md pb-16 text-sm text-slate-500 sm:pb-0">
-        <p>Built with <span className="hover-highlight">Next.js</span> and <span className="hover-highlight">Tailwind CSS</span>, deployed with <span className="hover-highlight">Netlify</span>. All text is set in the <span className="hover-highlight">Inter</span> typeface.</p>
+        <p>Built with <span className="hover-highlight">Next.js</span> and <span className="hover-highlight">Tailwind CSS</span>, deployed with <span className="hover-highlight">Vercel</span>. All text is set in the <span className="hover-highlight">Inter</span> typeface.</p>
     </footer>
   )
 }
