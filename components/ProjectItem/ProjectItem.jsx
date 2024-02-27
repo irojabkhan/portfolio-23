4ee1f80d100eb9d076aa4687c0b602c801292ea9
irojabkhan <irojabkhan@gmail.com>
2024-02-27 17:51:02 +0600

    vercel analytics added

--- components/ProjectItem/ProjectItem.jsx
+++ components/ProjectItem/ProjectItem.jsx
@@ -1,33 +1,48 @@
-import React from 'react'
-import { RightArrow } from '../Icons/Icons'
-import Badge from '../Badge/Badge'
+import React from "react";
+import { UpArrow } from "../Icons/Icons";
+import Badge from "../Badge/Badge";
 
-function ProjectItem({thumbnail, title, description, tags}) {
+function ProjectItem({ thumbnail, title, description, tags }) {
   return (
     <li className="mb-12">
-        <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
-            <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+      <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+        <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
 
-            <img src={thumbnail} alt="Build a Spotify Connected App Newline course marketing card" loading="lazy" width="200" height="48" decoding="async" data-nimg="1" className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:col-span-2 sm:translate-y-1" />
-            <div className="z-10 sm:col-span-6">
-                <h3>
-                    <a className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base" href="#" target="_blank">
-                        <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                        <span>{title} <RightArrow /></span>
-                    </a>
-                </h3>
-                <p className="mt-2 text-sm leading-normal">{description}</p>
-                <ul className="mt-2 flex flex-wrap" aria-label="Technologies used:">
-                    {tags.map((tag, index) => (
-                        <li className="mr-1.5 mt-2" key={index}>
-                            <Badge title={tag} />
-                        </li>
-                    ))}
-                </ul>
-            </div>
+        <img
+          src={thumbnail}
+          alt="Build a Spotify Connected App Newline course marketing card"
+          loading="lazy"
+          width="200"
+          height="48"
+          decoding="async"
+          data-nimg="1"
+          className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:col-span-2 sm:translate-y-1"
+        />
+        <div className="z-10 sm:col-span-6">
+          <h3>
+            <a
+              className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+              href="#"
+              target="_blank"
+            >
+              <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+              <span>
+                {title} <UpArrow />
+              </span>
+            </a>
+          </h3>
+          <p className="mt-2 text-sm leading-normal">{description}</p>
+          <ul className="mt-2 flex flex-wrap" aria-label="Technologies used:">
+            {tags.map((tag, index) => (
+              <li className="mr-1.5 mt-2" key={index}>
+                <Badge title={tag} />
+              </li>
+            ))}
+          </ul>
         </div>
+      </div>
     </li>
-  )
+  );
 }
 
-export default ProjectItem
+export default ProjectItem;
--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Document() {
   return (
-    <Html lang="en" className='scroll-smooth'>
+    <Html lang="en" className="scroll-smooth">
       <Head />
-      <body className='bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900'>
+      <body className="bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900">
         <Main />
         <NextScript />
+        <Analytics />
       </body>
     </Html>
-  )
+  );
 }
